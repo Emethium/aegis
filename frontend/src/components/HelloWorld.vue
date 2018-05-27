ab84112f977b79ff9068ee42be95f4141b7bac65
emethium <nikolas.serafini@gmail.com>
2018-05-27 19:31:00 -0300

    Table completed, music plays

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -22,6 +22,7 @@
             <li><a>Role-playing</a></li>
             <li><a>Racing</a></li>
             <li><a>Simulation</a></li>
+            <li><a>Survivor Horror</a></li>
             <li><a>Strategy</a></li>
             <li><a>Sports</a></li>
           </ul>
@@ -30,7 +31,7 @@
       <!-- Main content -->
       <div class="collumn has-background-light">
         <div class="content is-centered">
-          <table class="table">
+          <table class="table is-striped is-bordered is-hoverable is-narrow">
             <thead>
               <th class="has-text-primary has-text-centered">Number</th>
               <th class="has-text-primary has-text-centered">Title</th>
@@ -41,19 +42,136 @@
               <th class="has-text-primary has-text-centered">Play</th>
             </thead>
             <tbody>
-              <td class="has-text-weight-normal">1</td>
-              <td class="has-text-weight-normal">Stickerbrush Symphony</td>
-              <td class="has-text-weight-normal">Donkey Kong Country 2: Diddy's Kong Quest</td>
-              <td class="has-text-weight-normal">David Wise</td>
-              <td class="has-text-weight-normal">Platform</td>
-              <td class="has-text-weight-normal">Super Nintendo Entertainment System</td>
-              <td>
-                <av-line
-                  :line-width="1"
-                  line-color="lime"
-                  audio-src="https://cs1.mp3.pm/download/85870084/M0NsaUt5cTh2bFk0N3lXTUZ3UHovVThsQTZicVpMVGp6UnI3U1FHL2o4azJFT2dSK2R3Q0R1ZmlNL3ZoQUswOUg3bUI1VlIyV3RsV1M3eGo5OEIwdE93SDM2VzVBNUFuWmFpQ09DT3hFZm5NbG5oTXAwejBxeEVLSkdjL2psM2Y/Donkey_Kong_Country_2_-_17._Stickerbrush_Symphony_(mp3.pm).mp3">
-                </av-line>
-              </td>
+              <tr>
+                <td class="has-text-weight-normal">1</td>
+                <td class="has-text-weight-normal">Stickerbrush Symphony</td>
+                <td class="has-text-weight-normal">Donkey Kong Country 2: Diddy's Kong Quest</td>
+                <td class="has-text-weight-normal">David Wise</td>
+                <td class="has-text-weight-normal">Platform</td>
+                <td class="has-text-weight-normal">Super Nintendo Entertainment System</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/donkey-kong-country-02-diddys-kong-quest/dxdjmamx/17.%20Stickerbush%20Symphony.mp3" ref="foo" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">2</td>
+                <td class="has-text-weight-normal">Zero Opening Stage</td>
+                <td class="has-text-weight-normal">Megaman X: Corrupted</td>
+                <td class="has-text-weight-normal">NitroFlasher</td>
+                <td class="has-text-weight-normal">Platform</td>
+                <td class="has-text-weight-normal">Personal Computer (PC)</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/megaman-x-corrupted/lderepew/05%20-%20Zero%20Opening%20Stage.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">3</td>
+                <td class="has-text-weight-normal">Mercury Lighthouse - Outside</td>
+                <td class="has-text-weight-normal">Golden Sun</td>
+                <td class="has-text-weight-normal">Unknown</td>
+                <td class="has-text-weight-normal">RPG</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/golden-sun/ueobzuur/25%20mercury%20lighthouse%20-%20outside%20venus%20lighthouse.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">4</td>
+                <td class="has-text-weight-normal">Theme of Laura</td>
+                <td class="has-text-weight-normal">Silent Hill 2</td>
+                <td class="has-text-weight-normal">Akira Yamaoka</td>
+                <td class="has-text-weight-normal">Survivor Horror</td>
+                <td class="has-text-weight-normal">Sony Playstation 2</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/silent-hill-2-original-soundtrack/kkosmqvu/01%20-%20Theme%20Of%20Laura.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="has-text-weight-normal">5</td>
+                <td class="has-text-weight-normal">Heart of Fire</td>
+                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
+                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
+                <td class="has-text-weight-normal">Metroidvania</td>
+                <td class="has-text-weight-normal">Gameboy Advanced</td>
+                <td>
+                  <div style="padding-top: 1em;">
+                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
+                  </div>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
@@ -80,6 +198,10 @@
 </template>
 
 <style>
+.audio  {
+  position: absolute;
+  padding: 1.6em;
+}
 </style>
 
 <script>
