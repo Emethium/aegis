fb8bde4d80368107cf17e89314927b19892214ab
emethium <nikolas.serafini@gmail.com>
2018-05-28 11:06:06 -0300

    added json to store song data

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -36,162 +36,28 @@
               <th class="has-text-primary has-text-centered">Number</th>
               <th class="has-text-primary has-text-centered">Title</th>
               <th class="has-text-primary has-text-centered">Game</th>
-              <th class="has-text-primary has-text-centered">Artist</th>
+              <th class="has-text-primary has-text-centered">Composer</th>
               <th class="has-text-primary has-text-centered">Genre</th>
               <th class="has-text-primary has-text-centered">Platform</th>
               <th class="has-text-primary has-text-centered">Play</th>
             </thead>
             <tbody>
-              <tr>
-                <td class="has-text-weight-normal">1</td>
-                <td class="has-text-weight-normal">Stickerbrush Symphony</td>
-                <td class="has-text-weight-normal">Donkey Kong Country 2: Diddy's Kong Quest</td>
-                <td class="has-text-weight-normal">David Wise</td>
-                <td class="has-text-weight-normal">Platform</td>
-                <td class="has-text-weight-normal">Super Nintendo Entertainment System</td>
+              <tr v-for="s in this.songList" :key="s.id">
+                <td class="has-text-weight-normal">{{s.id}}</td>
+                <td class="has-text-weight-normal">{{s.title}}</td>
+                <td class="has-text-weight-normal">{{s.game}}</td>
+                <td class="has-text-weight-normal">{{s.composer}}</td>
+                <td class="has-text-weight-normal">{{s.genre}}</td>
+                <td class="has-text-weight-normal">{{s.platform}}</td>
                 <td>
                   <div style="padding-top: 1em;">
                     <audio src="http://66.90.93.122/ost/donkey-kong-country-02-diddys-kong-quest/dxdjmamx/17.%20Stickerbush%20Symphony.mp3" ref="foo" controls="true"></audio>
                   </div>
                 </td>
               </tr>
-              <tr>
-                <td class="has-text-weight-normal">2</td>
-                <td class="has-text-weight-normal">Zero Opening Stage</td>
-                <td class="has-text-weight-normal">Megaman X: Corrupted</td>
-                <td class="has-text-weight-normal">NitroFlasher</td>
-                <td class="has-text-weight-normal">Platform</td>
-                <td class="has-text-weight-normal">Personal Computer (PC)</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/megaman-x-corrupted/lderepew/05%20-%20Zero%20Opening%20Stage.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">3</td>
-                <td class="has-text-weight-normal">Mercury Lighthouse - Outside</td>
-                <td class="has-text-weight-normal">Golden Sun</td>
-                <td class="has-text-weight-normal">Unknown</td>
-                <td class="has-text-weight-normal">RPG</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/golden-sun/ueobzuur/25%20mercury%20lighthouse%20-%20outside%20venus%20lighthouse.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">4</td>
-                <td class="has-text-weight-normal">Theme of Laura</td>
-                <td class="has-text-weight-normal">Silent Hill 2</td>
-                <td class="has-text-weight-normal">Akira Yamaoka</td>
-                <td class="has-text-weight-normal">Survivor Horror</td>
-                <td class="has-text-weight-normal">Sony Playstation 2</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/silent-hill-2-original-soundtrack/kkosmqvu/01%20-%20Theme%20Of%20Laura.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td class="has-text-weight-normal">5</td>
-                <td class="has-text-weight-normal">Heart of Fire</td>
-                <td class="has-text-weight-normal">Castlevania Aria of Sorrow</td>
-                <td class="has-text-weight-normal">Konami Kukeiha Club</td>
-                <td class="has-text-weight-normal">Metroidvania</td>
-                <td class="has-text-weight-normal">Gameboy Advanced</td>
-                <td>
-                  <div style="padding-top: 1em;">
-                    <audio src="http://66.90.93.122/ost/castlevania-aria-of-sorrow/yyfupgde/23%20-%20heart%20of%20fire.mp3" ref="foo2" controls="true"></audio>
-                  </div>
-                </td>
-              </tr>
             </tbody>
           </table>
         </div>
-        <!--
-        <div class="media">
-          <div class="tile is-pulled-right">
-            <article class="tile is-child notification is-light">
-              <aplayer
-                theme="pic"
-                shuffle
-                repeat="list"
-                :controls="controls"
-                :muted.sync="muted"
-                :music.sync='music'
-                :list='list'
-              />    
-            </article>
-          </div>
-        </div>
-        -->
       </div>
     </div>
   </div>
@@ -205,51 +71,20 @@
 </style>
 
 <script>
-import Aplayer from "vue-aplayer";
 import AudioVisual from 'vue-audio-visual'
+import songs from '../assets/data.json'
 
 export default {
   name: "HelloWorld",
   components: {
-    Aplayer,
     AudioVisual
   },
-  props: {
-    msg: String
-  },
   created() {
-    this.music = this.list[0];
+    console.log(this.songList)
   },
   data() {
     return {
-      controls: false,
-      muted: false,
-      music3: null,
-      list: [
-        {
-          title: "Zero Opening Stage",
-          artist: "Mega Man X Corrupted",
-          src:
-            "http://66.90.93.122/ost/megaman-x-corrupted/lderepew/05%20-%20Zero%20Opening%20Stage.mp3",
-          pic: "https://i.ytimg.com/vi/GQ47immxaQ8/hqdefault.jpg"
-        },
-        {
-          title: "Stickerbrush Symphony",
-          artist: "Donkey Kong Country 2",
-          src:
-            "https://cs1.mp3.pm/download/85870084/M0NsaUt5cTh2bFk0N3lXTUZ3UHovVThsQTZicVpMVGp6UnI3U1FHL2o4azJFT2dSK2R3Q0R1ZmlNL3ZoQUswOUg3bUI1VlIyV3RsV1M3eGo5OEIwdE93SDM2VzVBNUFuWmFpQ09DT3hFZm5NbG5oTXAwejBxeEVLSkdjL2psM2Y/Donkey_Kong_Country_2_-_17._Stickerbrush_Symphony_(mp3.pm).mp3",
-          pic:
-            "https://cdn02.nintendo-europe.com/media/images/03_teaser_module_1_square/games_3/game_boy_advance_5/TM_GBA_DonkeyKongCountry2.png"
-        },
-        {
-          title: "Into the Wilderness",
-          artist: "Wild Arms",
-          src:
-            "http://66.90.93.122/ost/wild-arms-original-game-soundtrack/kfbkgbxc/01%20into%20the%20wilderness.mp3",
-          pic:
-            "http://images2.wikia.nocookie.net/__cb20080424154713/wildarms/images/3/3d/Wildarms.jpg"
-        }
-      ]
+      songList: songs
     };
   }
 };
